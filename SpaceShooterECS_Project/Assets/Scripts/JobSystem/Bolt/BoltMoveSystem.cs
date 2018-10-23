c7c2a14a7ad21cfcb5ed1b12fdd36657d0f95444
Unknown <issam.khalil@intel.com>
2018-10-22 18:01:29 -0700

    Move to ArchetypeChunk code from componentDataArray

--- SpaceShooterECS_Project/Assets/Scripts/JobSystem/Bolt/BoltMoveSystem.cs
+++ SpaceShooterECS_Project/Assets/Scripts/JobSystem/Bolt/BoltMoveSystem.cs
@@ -10,80 +10,117 @@ namespace ECS_SpaceShooterDemo
     [UpdateBefore(typeof(EntityToInstanceRendererTransform))]
     public class BoltMoveSystem : GameControllerJobComponentSystem
     {
-        struct BoltMoveDataGroup
+        private ComponentGroup  boltMoveDataComponentGroup;
+           
+        [BurstCompile]
+        struct BoltMoveJobChunck : IJobParallelFor
         {
-            public ComponentDataArray<BoltMoveData> boltMoveDataArray;
-            public ComponentDataArray<EntityInstanceRenderData> entityInstanceRenderDataArray;
-            public ComponentDataArray<EntityBoundCenterData> entityBoundCenterDataArray;
-            public ComponentDataArray<EntityBoundMinMaxData> entityBoundMinMaxDataArray;
-            [ReadOnly]
-            public ComponentDataArray<EntityBoundOffsetData> entityBoundOffsetDataArray;
-            [ReadOnly]
-            public ComponentDataArray<EntityBoundExtendData> entityBoundExtendDataArray;
+            [DeallocateOnJobCompletion] [ReadOnly] public NativeArray<ArchetypeChunk> chunks;
             
-            public readonly int Length; //required variable
-        }
-        [Inject]
-        BoltMoveDataGroup boltMoveDataGroup;
-
-        [BurstCompileAttribute(Accuracy.Med, Support.Relaxed)]
-        struct BoltMoveJob : IJobParallelFor
-        {
-            public ComponentDataArray<BoltMoveData> boltMoveDataArray;
-            public ComponentDataArray<EntityInstanceRenderData> entityInstanceRenderDataArray;
-            public ComponentDataArray<EntityBoundCenterData> entityBoundCenterDataArray;
-            public ComponentDataArray<EntityBoundMinMaxData> entityBoundMinMaxDataArray;
-            [ReadOnly]
-            public ComponentDataArray<EntityBoundOffsetData> entityBoundOffsetDataArray;
-            [ReadOnly]
-            public ComponentDataArray<EntityBoundExtendData> entityBoundExtendDataArray;
+            public ArchetypeChunkComponentType<BoltMoveData> boltMoveDataRW;
+            public ArchetypeChunkComponentType<EntityInstanceRenderData> renderDataRW;
+            public ArchetypeChunkComponentType<EntityBoundCenterData> boundCenterDataRW;
+            public ArchetypeChunkComponentType<EntityBoundMinMaxData> boundMinMaxDataRW;
+            [ReadOnly] public ArchetypeChunkComponentType<EntityBoundOffsetData> boundOffsetDataRO;
+            [ReadOnly] public ArchetypeChunkComponentType<EntityBoundExtendData> boundExtendDataRO;
+            
             public float deltaTime;
             public float3 renderDataForward;
-
-            public void Execute(int index)
+            
+            public void Execute(int chunkIndex)
             {
-                BoltMoveData boltMoveData = boltMoveDataArray[index];
-                boltMoveData.position += (boltMoveData.speed * boltMoveData.forwardDirection * deltaTime);
-                boltMoveDataArray[index] = boltMoveData;
-
-                EntityInstanceRenderData entityInstanceRenderData = entityInstanceRenderDataArray[index];
-                entityInstanceRenderData.position = boltMoveData.position;
-                entityInstanceRenderData.forward = renderDataForward;
-
-                entityInstanceRenderData.up = -boltMoveData.forwardDirection;
-
-                entityInstanceRenderDataArray[index] = entityInstanceRenderData;
-
-                EntityBoundCenterData entityBoundCenterData = entityBoundCenterDataArray[index];
-                EntityBoundMinMaxData entityBoundMinMaxData = entityBoundMinMaxDataArray[index];
-
-                entityBoundCenterData.centerPosition = boltMoveData.position + entityBoundOffsetDataArray[index].offset;
-                entityBoundMinMaxData.min = entityBoundCenterData.centerPosition - entityBoundExtendDataArray[index].extend;
-                entityBoundMinMaxData.max = entityBoundCenterData.centerPosition + entityBoundExtendDataArray[index].extend;
+                ArchetypeChunk chunk = chunks[chunkIndex];
+                int dataCount = chunk.Count;
+                
+                NativeArray<BoltMoveData> boltMoveDataArray = chunk.GetNativeArray(boltMoveDataRW);
+                NativeArray<EntityInstanceRenderData> renderDataArray = chunk.GetNativeArray(renderDataRW);
+                NativeArray<EntityBoundCenterData> boundCenterDataArray = chunk.GetNativeArray(boundCenterDataRW);
+                NativeArray<EntityBoundMinMaxData> boundMinMaxDataArray = chunk.GetNativeArray(boundMinMaxDataRW);
+                NativeArray<EntityBoundOffsetData> boundOffsetDataArray = chunk.GetNativeArray(boundOffsetDataRO);
+                NativeArray<EntityBoundExtendData> boundExtendDataArray = chunk.GetNativeArray(boundExtendDataRO);
 
 
-                entityBoundCenterDataArray[index] = entityBoundCenterData;
-                entityBoundMinMaxDataArray[index] = entityBoundMinMaxData;
+                for (int dataIndex = 0; dataIndex < dataCount; dataIndex++)
+                {
+                    BoltMoveData boltMoveData = boltMoveDataArray[dataIndex];
+                    boltMoveData.position += (boltMoveData.speed * boltMoveData.forwardDirection * deltaTime);
+                    boltMoveDataArray[dataIndex] = boltMoveData;
+
+                    EntityInstanceRenderData entityInstanceRenderData = renderDataArray[dataIndex];
+                    entityInstanceRenderData.position = boltMoveData.position;
+                    entityInstanceRenderData.forward = renderDataForward;
+
+                    entityInstanceRenderData.up = -boltMoveData.forwardDirection;
+
+                    renderDataArray[dataIndex] = entityInstanceRenderData;
+
+                    EntityBoundCenterData entityBoundCenterData = boundCenterDataArray[dataIndex];
+                    EntityBoundMinMaxData entityBoundMinMaxData = boundMinMaxDataArray[dataIndex];
+
+                    entityBoundCenterData.centerPosition = boltMoveData.position + boundOffsetDataArray[dataIndex].offset;
+                    entityBoundMinMaxData.min = entityBoundCenterData.centerPosition - boundExtendDataArray[dataIndex].extend;
+                    entityBoundMinMaxData.max = entityBoundCenterData.centerPosition + boundExtendDataArray[dataIndex].extend;
+
+
+                    boundCenterDataArray[dataIndex] = entityBoundCenterData;
+                    boundMinMaxDataArray[dataIndex] = entityBoundMinMaxData;
+                }
             }
         }
+      
+        protected override void OnCreateManager()
+        {
+            base.OnCreateManager();
+            boltMoveDataComponentGroup = GetComponentGroup(new EntityArchetypeQuery
+            {
+                Any = new ComponentType[] {}, 
+                None = new ComponentType[] {},
+                All = new ComponentType[]
+                {
+                    typeof(BoltMoveData), 
+                    typeof(EntityInstanceRenderData), 
+                    typeof(EntityBoundCenterData),
+                    typeof(EntityBoundMinMaxData),
+                    typeof(EntityBoundOffsetData),
+                    typeof(EntityBoundExtendData),
+                },
+            });        
+        }
+
 
         protected override JobHandle OnUpdate(JobHandle inputDeps)
-        {
-            BoltMoveJob moveJob = new BoltMoveJob
+        {         
+            ArchetypeChunkComponentType<BoltMoveData> boltMoveDataRW = GetArchetypeChunkComponentType<BoltMoveData>(false);
+            ArchetypeChunkComponentType<EntityInstanceRenderData> renderDataRW = GetArchetypeChunkComponentType<EntityInstanceRenderData>(false);
+            ArchetypeChunkComponentType<EntityBoundCenterData> boundCenterDataRW = GetArchetypeChunkComponentType<EntityBoundCenterData>(false);
+            ArchetypeChunkComponentType<EntityBoundMinMaxData> boundMinMaxDataRW = GetArchetypeChunkComponentType<EntityBoundMinMaxData>(false);
+            ArchetypeChunkComponentType<EntityBoundOffsetData> boundOffsetDataRO = GetArchetypeChunkComponentType<EntityBoundOffsetData>(true);
+            ArchetypeChunkComponentType<EntityBoundExtendData> boundExtendDataRO = GetArchetypeChunkComponentType<EntityBoundExtendData>(true);
+            
+            
+            NativeArray<ArchetypeChunk> boltMoveDataChunk = boltMoveDataComponentGroup.CreateArchetypeChunkArray(Allocator.TempJob);
+            if (boltMoveDataChunk.Length == 0)
             {
-                boltMoveDataArray = boltMoveDataGroup.boltMoveDataArray,
-                entityInstanceRenderDataArray = boltMoveDataGroup.entityInstanceRenderDataArray,
-                entityBoundCenterDataArray = boltMoveDataGroup.entityBoundCenterDataArray,
-                entityBoundMinMaxDataArray = boltMoveDataGroup.entityBoundMinMaxDataArray,
-                entityBoundOffsetDataArray = boltMoveDataGroup.entityBoundOffsetDataArray,
-                entityBoundExtendDataArray = boltMoveDataGroup.entityBoundExtendDataArray,
+                boltMoveDataChunk.Dispose();
+                return inputDeps;
+            }
+
+            BoltMoveJobChunck moveJob = new BoltMoveJobChunck
+            {
+                chunks = boltMoveDataChunk,
+                boltMoveDataRW = boltMoveDataRW,
+                renderDataRW = renderDataRW,
+                boundCenterDataRW = boundCenterDataRW,
+                boundMinMaxDataRW = boundMinMaxDataRW,
+                boundOffsetDataRO = boundOffsetDataRO,
+                boundExtendDataRO = boundExtendDataRO,
                 deltaTime = Time.deltaTime,
                 renderDataForward = new float3(0,-1, 0),
             };
 
-            return moveJob.Schedule(boltMoveDataGroup.Length,
-                                    MonoBehaviourECSBridge.Instance.GetJobBatchCount(boltMoveDataGroup.Length),
-                                    inputDeps);
+            return moveJob.Schedule(boltMoveDataChunk.Length,
+                MonoBehaviourECSBridge.Instance.GetJobBatchCount(boltMoveDataChunk.Length),
+                inputDeps);
         }
     }
 }
