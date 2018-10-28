b4de1839cae4a5b043b86bff40d680ed88ef1319
Issam Khalil <issam.khalil@intel.com>
2018-10-28 12:34:05 -0500

    Updating the asteroid system
    Use the asteroid sample as the basic example of how to query/parse chunk arrays

--- SpaceShooterECS_Project/Assets/Scripts/JobSystem/Asteroid/AsteroidMoveSystem.cs
+++ SpaceShooterECS_Project/Assets/Scripts/JobSystem/Asteroid/AsteroidMoveSystem.cs
@@ -11,6 +11,8 @@ namespace ECS_SpaceShooterDemo
     [UpdateBefore(typeof(EntityToInstanceRendererTransform))]
     public class AsteroidMoveSystem : GameControllerJobComponentSystem
     {
+        private ComponentGroup  asteroidMoveDataComponentGroup;
+        
         struct AsteroidModeDataGroup
         {
             public ComponentDataArray<AsteroidMoveData> asteroidMoveDataArray;
@@ -27,80 +29,143 @@ namespace ECS_SpaceShooterDemo
         [Inject]
         AsteroidModeDataGroup asteroidMoveDataGroup;
 
-        [BurstCompileAttribute(Accuracy.Med, Support.Relaxed)]
-        struct AsteroidModeJob : IJobParallelFor
+        
+        [BurstCompile]
+        struct AsteroidMoveJob : IJobParallelFor
         {
-            public ComponentDataArray<AsteroidMoveData> asteroidMoveDataArray;
-            public ComponentDataArray<EntityInstanceRenderData> entityInstanceRenderDataArray;
-            public ComponentDataArray<EntityBoundCenterData> entityBoundCenterDataArray;
-            public ComponentDataArray<EntityBoundMinMaxData> entityBoundMinMaxDataArray;
-            [ReadOnly]
-            public ComponentDataArray<EntityBoundOffsetData> entityBoundOffsetDataArray;
-            [ReadOnly]
-            public ComponentDataArray<EntityBoundExtendData> entityBoundExtendDataArray;
+            [DeallocateOnJobCompletion] [ReadOnly] public NativeArray<ArchetypeChunk> chunks;
+            
+            public ArchetypeChunkComponentType<AsteroidMoveData> asteroidMoveDataRW;
+            public ArchetypeChunkComponentType<EntityInstanceRenderData> renderDataRW;
+            public ArchetypeChunkComponentType<EntityBoundCenterData> boundCenterDataRW;
+            public ArchetypeChunkComponentType<EntityBoundMinMaxData> boundMinMaxDataRW;
+            [ReadOnly] public ArchetypeChunkComponentType<EntityBoundOffsetData> boundOffsetDataRO;
+            [ReadOnly] public ArchetypeChunkComponentType<EntityBoundExtendData> boundExtendDataRO;
+            
             public float deltaTime;
-
-            public void Execute(int index)
+          
+            public void Execute(int chunkIndex)
             {
-                AsteroidMoveData asteroidMoveData = asteroidMoveDataArray[index];
-                asteroidMoveData.position += (asteroidMoveData.speed * asteroidMoveData.forwardDirection * deltaTime);
-
-                //https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
-                float rotationAngle = Mathf.Deg2Rad * asteroidMoveData.rotationSpeed * deltaTime;
-                float cosValue = math.cos(rotationAngle);
-                float sinValue = math.sin(rotationAngle);
-                float3 crossVector = math.cross(asteroidMoveData.rotationAxis, asteroidMoveData.renderForward);
-                float dotValue = math.dot(asteroidMoveData.rotationAxis, asteroidMoveData.renderForward);
-
-
-
-                asteroidMoveData.renderForward = (asteroidMoveData.renderForward * cosValue)
-                                                    + (crossVector * sinValue)
-                                                    + (asteroidMoveData.rotationAxis * dotValue * (1.0f - cosValue));
+                ArchetypeChunk chunk = chunks[chunkIndex];
+                int dataCount = chunk.Count;
+                
+                NativeArray<AsteroidMoveData> asteroidMoveDataArray = chunk.GetNativeArray(asteroidMoveDataRW);
+                NativeArray<EntityInstanceRenderData> renderDataArray = chunk.GetNativeArray(renderDataRW);
+                NativeArray<EntityBoundCenterData> boundCenterDataArray = chunk.GetNativeArray(boundCenterDataRW);
+                NativeArray<EntityBoundMinMaxData> boundMinMaxDataArray = chunk.GetNativeArray(boundMinMaxDataRW);
+                NativeArray<EntityBoundOffsetData> boundOffsetDataArray = chunk.GetNativeArray(boundOffsetDataRO);
+                NativeArray<EntityBoundExtendData> boundExtendDataArray = chunk.GetNativeArray(boundExtendDataRO);
 
 
-                asteroidMoveDataArray[index] = asteroidMoveData;
-
-                EntityInstanceRenderData entityInstanceRenderData = entityInstanceRenderDataArray[index];
-
-                entityInstanceRenderData.position = asteroidMoveData.position;
-                entityInstanceRenderData.forward = asteroidMoveData.renderForward;
-                entityInstanceRenderData.up = new float3(0, 1, 0);
-
-                entityInstanceRenderDataArray[index] = entityInstanceRenderData;
-
-                EntityBoundCenterData entityBoundCenterData = entityBoundCenterDataArray[index];
-                EntityBoundMinMaxData entityBoundMinMaxData = entityBoundMinMaxDataArray[index];
-
-                entityBoundCenterData.centerPosition = asteroidMoveData.position + entityBoundOffsetDataArray[index].offset;
-                entityBoundMinMaxData.min = entityBoundCenterData.centerPosition - entityBoundExtendDataArray[index].extend;
-                entityBoundMinMaxData.max = entityBoundCenterData.centerPosition + entityBoundExtendDataArray[index].extend;
-
-
-                entityBoundCenterDataArray[index] = entityBoundCenterData;
-                entityBoundMinMaxDataArray[index] = entityBoundMinMaxData;
-
+                for (int dataIndex = 0; dataIndex < dataCount; dataIndex++)
+                {
+                    
+                    AsteroidMoveData asteroidMoveData = asteroidMoveDataArray[dataIndex];
+                    asteroidMoveData.position += (asteroidMoveData.speed * asteroidMoveData.forwardDirection * deltaTime);
+    
+                    //https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
+                    float rotationAngle = Mathf.Deg2Rad * asteroidMoveData.rotationSpeed * deltaTime;
+                    float cosValue = math.cos(rotationAngle);
+                    float sinValue = math.sin(rotationAngle);
+                    float3 crossVector = math.cross(asteroidMoveData.rotationAxis, asteroidMoveData.renderForward);
+                    float dotValue = math.dot(asteroidMoveData.rotationAxis, asteroidMoveData.renderForward);
+    
+    
+    
+                    asteroidMoveData.renderForward = (asteroidMoveData.renderForward * cosValue)
+                                                        + (crossVector * sinValue)
+                                                        + (asteroidMoveData.rotationAxis * dotValue * (1.0f - cosValue));
+    
+    
+                    asteroidMoveDataArray[dataIndex] = asteroidMoveData;
+    
+                    EntityInstanceRenderData entityInstanceRenderData = renderDataArray[dataIndex];
+    
+                    entityInstanceRenderData.position = asteroidMoveData.position;
+                    entityInstanceRenderData.forward = asteroidMoveData.renderForward;
+                    entityInstanceRenderData.up = new float3(0, 1, 0);
+    
+                    renderDataArray[dataIndex] = entityInstanceRenderData;
+    
+                    EntityBoundCenterData entityBoundCenterData = boundCenterDataArray[dataIndex];
+                    EntityBoundMinMaxData entityBoundMinMaxData = boundMinMaxDataArray[dataIndex];
+    
+                    entityBoundCenterData.centerPosition = asteroidMoveData.position + boundOffsetDataArray[dataIndex].offset;
+                    entityBoundMinMaxData.min = entityBoundCenterData.centerPosition - boundExtendDataArray[dataIndex].extend;
+                    entityBoundMinMaxData.max = entityBoundCenterData.centerPosition + boundExtendDataArray[dataIndex].extend;
+    
+    
+                    boundCenterDataArray[dataIndex] = entityBoundCenterData;
+                    boundMinMaxDataArray[dataIndex] = entityBoundMinMaxData;                    
+                    
+                }
             }
         }
 
 
-        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        protected override void OnCreateManager()
         {
-            var moveJob = new AsteroidModeJob
+            base.OnCreateManager();
+            asteroidMoveDataComponentGroup = GetComponentGroup(new EntityArchetypeQuery
             {
-                asteroidMoveDataArray = asteroidMoveDataGroup.asteroidMoveDataArray,
-                entityInstanceRenderDataArray = asteroidMoveDataGroup.entityInstanceRenderDataArray,
-                entityBoundCenterDataArray = asteroidMoveDataGroup.entityBoundCenterDataArray,
-                entityBoundMinMaxDataArray = asteroidMoveDataGroup.entityBoundMinMaxDataArray,
-                entityBoundOffsetDataArray = asteroidMoveDataGroup.entityBoundOffsetDataArray,
-                entityBoundExtendDataArray = asteroidMoveDataGroup.entityBoundExtendDataArray,
-                deltaTime = Time.deltaTime
+                Any = new ComponentType[] {}, 
+                None = new ComponentType[] {},
+                All = new ComponentType[]
+                {
+                    typeof(AsteroidMoveData), 
+                    typeof(EntityInstanceRenderData), 
+                    typeof(EntityBoundCenterData),
+                    typeof(EntityBoundMinMaxData),
+                    typeof(EntityBoundOffsetData),
+                    typeof(EntityBoundExtendData),
+                },
+            }); 
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {            
+            ArchetypeChunkComponentType<AsteroidMoveData> asteroidMoveDataRW = GetArchetypeChunkComponentType<AsteroidMoveData>(false);
+            ArchetypeChunkComponentType<EntityInstanceRenderData> renderDataRW = GetArchetypeChunkComponentType<EntityInstanceRenderData>(false);
+            ArchetypeChunkComponentType<EntityBoundCenterData> boundCenterDataRW = GetArchetypeChunkComponentType<EntityBoundCenterData>(false);
+            ArchetypeChunkComponentType<EntityBoundMinMaxData> boundMinMaxDataRW = GetArchetypeChunkComponentType<EntityBoundMinMaxData>(false);
+            ArchetypeChunkComponentType<EntityBoundOffsetData> boundOffsetDataRO = GetArchetypeChunkComponentType<EntityBoundOffsetData>(true);
+            ArchetypeChunkComponentType<EntityBoundExtendData> boundExtendDataRO = GetArchetypeChunkComponentType<EntityBoundExtendData>(true);
+
+
+            //CreateArchetypeChunkArray runs inside a job, we can use a job handle to make dependency on that job
+            //A NativeArray<ArchetypeChunk> is allocated with teh correct size on the main thread and that's what is returned, we are responsible for de-allocating it (In this case using [DeallocateOnJobCompletion] in the move job)
+            //The job scheduled by CreateArchetypeChunkArray fill that array with correct chunk information
+            JobHandle createChunckArrayJobHandle; 
+            NativeArray<ArchetypeChunk> asteroidMoveDataChunk = asteroidMoveDataComponentGroup.CreateArchetypeChunkArray(Allocator.TempJob, out createChunckArrayJobHandle);
+            
+            //Special case when our query return no chunk at all
+            if (asteroidMoveDataChunk.Length == 0)
+            {
+                createChunckArrayJobHandle.Complete();
+                asteroidMoveDataChunk.Dispose();
+                return inputDeps;
+            }
+            
+            
+            //Make sure our movejob is dependent on the job filling the array has completed
+            JobHandle moveJobDependency = JobHandle.CombineDependencies(inputDeps, createChunckArrayJobHandle);
+            
+            AsteroidMoveJob moveJob = new AsteroidMoveJob
+            {
+                chunks = asteroidMoveDataChunk,
+                asteroidMoveDataRW = asteroidMoveDataRW,
+                renderDataRW = renderDataRW,
+                boundCenterDataRW = boundCenterDataRW,
+                boundMinMaxDataRW = boundMinMaxDataRW,
+                boundOffsetDataRO = boundOffsetDataRO,
+                boundExtendDataRO = boundExtendDataRO,
+                deltaTime = Time.deltaTime,
             };
 
-
-            return moveJob.Schedule(asteroidMoveDataGroup.Length,
-                                    MonoBehaviourECSBridge.Instance.GetJobBatchCount(asteroidMoveDataGroup.Length),
-                                    inputDeps);
+            return moveJob.Schedule(asteroidMoveDataChunk.Length,
+                MonoBehaviourECSBridge.Instance.GetJobBatchCount(asteroidMoveDataChunk.Length),
+                moveJobDependency);            
+            
         }
     }
 }
